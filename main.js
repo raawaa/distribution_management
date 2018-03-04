0a4a0111fb95f904af4c3557a0db6dcf45721c9e
raawaa <wenjie_yu@yeah.net>
2018-03-05 02:25:39 +0800

    implement searching

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const url = require('url')
 
+const TEST_DB_NAME = "database_test.db";
+const DB_DIRNAME = "DATA";
+const DATA_SOURCE_DIRNAME = "data_source";
+const STATION_DIRNAME = "station";
+const DISTRIBUTION_ROOM_DIRNAME = "distribution_room";
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: path.join(__dirname, DB_DIRNAME, TEST_DB_NAME)
+    },
+    useNullAsDefault: true
+});
 let win;
 
 const createWindow = () => {
@@ -13,7 +25,7 @@ const createWindow = () => {
         acceptFirstMouse: true
     });
     win.loadURL(url.format({
-        pathname: path.join(__dirname, 'src','index.html'),
+        pathname: path.join(__dirname, 'src', 'index.html'),
         protocol: 'file',
         slashes: true
     }));
@@ -34,4 +46,10 @@ app.on('activate', () => {
     if (win === null) {
         createWindow();
     }
+})
+
+ipcMain.on('search', (event, arg) => {
+    knex.select().from('device_node').where('name', 'like', '%' + arg + '%').timeout(2000).then(res => {
+        event.sender.send('search-result', res);
+    })
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
+const { remote, ipcRenderer } = require('electron')
+const Menu = remote.Menu
+const MenuItem = remote.MenuItem
+
 const Vue = require('vue')
-console.log(Vue)
+
 const app = new Vue({
-    el: '#app',
-    data: {
-        message: 'lellajsdfasdf'
+  el: '#app',
+  data: {
+    message: 'lellajsdfasdf',
+    searchResults: null
+  },
+  methods: {
+    fetchData: function (event) {
+      ipcRenderer.send('search',event.target.value)
     }
+  }
 })
+
+ipcRenderer.on('search-result',(event,arg)=>{
+  app.searchResults=arg;
+})
+
+// Build our new menu
+var menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Delete',
+  click: function () {
+    // Trigger an alert when menu item is clicked
+    alert('Deleted')
+  }
+}))
+menu.append(new MenuItem({
+  label: 'More Info...',
+  click: function () {
+    // Trigger an alert when menu item is clicked
+    alert('Here is more information')
+  }
+}))
+
+// Add the listener
+window.addEventListener('contextmenu', e => {
+  e.preventDefault();
+  menu.popup(remote.getCurrentWebContents())
+}, false)
